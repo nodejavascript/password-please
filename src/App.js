fe0b726fa22190aafd2819234d287a0782ba8a66
nodejavascript <support@nodejavascript.com>
2023-01-08 16:33:33 -0500

    show env

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import SiteLayout from './components/SiteLayout'
 
 const rootStyle = { height: '100vh' }
 
+const { NODE_ENV } = process.env
 const App = () => {
+  console.log('NODE_ENV', NODE_ENV)
   return (
     <div style={rootStyle}>
       <SiteLayout />
